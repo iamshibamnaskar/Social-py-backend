ced8ca3a6472dbb448eb8653e34f6978816c4623
iamshibamnaskar <sn2446@cse.jgec.ac.in>
2025-01-07 23:25:39 +0530

    fix cors

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const verifyToken = require("./middlewares/verifyToken");
 dotenv.config();
 
 app.use(cors({
-  origin: "*", // Allows all origins
+  origin: "http://localhost:5173",
+  credentials: true,
 }));
 
 // Middleware setup
