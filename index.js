f2177830d15a27e210e8bf8412f804175661af89
iamshibamnaskar <sn2446@cse.jgec.ac.in>
2025-01-08 00:21:31 +0530

    new

--- index.js
+++ index.js
@@ -41,8 +41,7 @@ app.use(cors({
   credentials: true,
 }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader("Access-Control-Allow-Origin", "https://your-frontend-vercel-domain.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
   res.setHeader("Access-Control-Allow-Credentials", true);
